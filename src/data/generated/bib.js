d6aa333bdab1059f99abb33d6cee7cd4af467eca
Morgan Jones <morganojones@yahoo.com>
2026-04-26 15:40:54 +0100

    Remove Beck2016Visual entry from bib.js
    Removed the bibliographic entry for Beck (2016) from generatedBibEntries.

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,19 +1,4 @@
 const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
-        "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
-    },
     "sathiyanarayanan2025progressive": {
         "abstract": "The success of the machine learning field has reliably depended on training on large datasets. While effective, this trend comes at an extraordinary cost. The proposed Progressive Data Dropout reduces the number of effective epochs to as little as 12.4% of the baseline without any cost to accuracy, and surprisingly improves accuracy by up to 4.82%.",
         "author": "Sathiyanarayanan, Shriram M and Hao, Xinyue and Hou, Shihao and Lu, Yang and Sevilla-Lara, Laura and Arnab, Anurag and Gowda, Shreyank N",
