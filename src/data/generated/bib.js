c290440be75308e275f29c70684253e8589e9956
Morgan Jones <morganojones@yahoo.com>
2026-04-26 16:17:08 +0100

    Add 'Curriculum Learning' entry to bib.js
    Added a new entry for 'Curriculum Learning' with details including abstract, authors, and publication information.

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -96,5 +96,18 @@ const generatedBibEntries = {
         "issn": "0360-0300",
         "volume": "57",
         "number": "3"
+    },
+    "10.1145/1553374.1553380": {
+        "abstract": "Humans and animals learn much better when the examples are not randomly presented but organized in a meaningful order which illustrates gradually more concepts, and gradually more complex ones. Here, we formalize such training strategies in the context of machine learning, and call them "curriculum learning". In the context of recent research studying the difficulty of training in the presence of non-convex training criteria (for deep deterministic and stochastic neural networks), we explore curriculum learning in various set-ups. The experiments show that significant improvements in generalization can be achieved. We hypothesize that curriculum learning has both an effect on the speed of convergence of the training process to a minimum and, in the case of non-convex criteria, on the quality of the local minima obtained: curriculum learning can be seen as a particular form of continuation method (a general strategy for global optimization of non-convex functions).",
+        "author": "Bengio, Yoshua and Louradour, Jerome and Collobert, Ronan and Weston, Jason",
+        "booktitle": "Proceedings of the 26th Annual International Conference on Machine Learning",
+        "doi": "10.1145/1553374.1553380",
+        "isbn": "9781605585161",
+        "keywords": "type: inproceedings, category: curriculum_learning, deep_learning",
+        "publisher": "Association for Computing Machinery",
+        "title": "Curriculum Learning", 
+        "url": "https://doi.org/10.1145/1553374.1553380",
+        "pages": "41-48",
+        "year": "2009"
     }
 };
